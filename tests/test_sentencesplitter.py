d86ac748d8d1fb77ec8314c76f32d7af25904f9f
Evaldas <Evaldas@evalkaz.com>
2017-10-16 20:41:33 +0300

    Added initial tests

--- tests/test_sentencesplitter.py
+++ tests/test_sentencesplitter.py
@@ -1,2 +1,31 @@
-def test_example():
-    assert 1 == 1
+# -*- coding: utf-8 -*-
+
+"""
+Unit tests for testing SentenceSplitter class
+"""
+
+
+from lltk.sentencesplitter import SentenceSplitter
+
+
+def test_sentencesplitter():
+    """
+    This test is to check how good sentence splitter is. It contains 4 tests:
+    - simple sentence,
+    - sentence that starts with abbreviation,
+    - multiple abbreviations in sentence,
+    - sentence with quote.
+    """
+
+    splitter = SentenceSplitter()
+
+    sentences = [
+        '''Labas vakaras.''',
+        '''Dr. Jonas Basanavičius buvo signataras.''',
+        '''Šiaulių apskr. Radvilišio raj. turi mažiau nei 100 tūkst.
+        gyventojų.''',
+        '''Kaip a.a. draugas A. Sipavičius sakė: \"Ne piniguose laimė, o jų
+        perkamojoje galioje.\"'''
+    ]
+
+    assert splitter.split(' '.join(sentences)) == sentences
